5c3931350d4d47d47a2f2fe5a4a07d412f25dd50
Jing Ai <ai.jing.cph@gmail.com>
2022-03-21 22:36:37 +0100

    fix-bug: proj1 ArrayDeque

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,134 +1,122 @@
 package deque;
 
 public class ArrayDeque<T> {
-    private T[] items;
-    private int frontIndex;
-    private int backIndex;
-    private int size;
+  private final double REFACTOR = 2.0;
+  protected int head;
+  protected int tail;
+  private T[] items;
+  private int size;
 
-    /** Creates an empty list. */
-    public ArrayDeque() {
-        this.items = (T[]) new Object[8];
-        this.frontIndex = 0;
-        this.backIndex = 0;
-        this.size = 0;
-    }
+  /** Creates an empty list. */
+  public ArrayDeque() {
+    this.items = (T[]) new Object[8];
+    this.head = 0;
+    this.tail = 1;
+    this.size = 0;
+  }
 
-    /** Inserts item into the front of the list. */
-    public void addFirst(T item){
-        if (size == 0) {
-            items[frontIndex] = item;
-            backIndex = 0;
-        }
-        else if (size < items.length) {
-            if (frontIndex != 0) {
-                frontIndex = frontIndex - 1;
-                items[frontIndex] = item;
-            }
-            else {
-                backIndex = backIndex + 1;
-                items[backIndex] = item;
-            }
-        }
-        else if (size == items.length) {
-            resize((int) (size * 1.25));
-            backIndex = backIndex + 1;
-            items[backIndex] = item;
-        }
-        size += 1;
+  /** Inserts item into the front of the list. */
+  public void addFirst(T item) {
+    items[head] = item;
+    head -= 1;
+    if (head == -1) {
+      head = items.length - 1;
     }
+    size += 1;
+    if (head == tail) {
+      resize((int) (items.length * REFACTOR));
+    }
+  }
 
-    /** Inserts X into the back of the list. */
-    public void addLast(T item) {
-        if (size == items.length) {
-            resize((int) (size * 1.25));
-        }
-        items[size] = item;
-        backIndex = size;
-        size = size + 1;
+  /** Inserts X into the back of the list. */
+  public void addLast(T item) {
+    items[tail] = item;
+    tail += 1;
+    if (tail == items.length) {
+      tail = 0;
     }
+    size += 1;
+    if (head == tail) {
+      resize((int) (items.length * REFACTOR));
+    }
+  }
 
-    /** Remove item from the front of the list. */
-    public T removeFirst() {
-        if(isEmpty()) return null;
-        T firstItem = items[frontIndex];
-        items[frontIndex] = null;
-        size -= 1;
-        if (size == 0) {
-            frontIndex = 0;
-            backIndex = 0;
-            resize(8);
-        }
-        else {
-            frontIndex += 1;
-            // resizing
-            if ((size < items.length / 4) && (size > 8) ) {
-                T[] a = (T[]) new Object[items.length / 4];
-                System.arraycopy(items, frontIndex, a, 0, size);
-                items = a;
-                frontIndex = 0;
-                backIndex = size - 1;
-            }
-        }
-        return firstItem;
-    }
+  /** Remove item from the front of the list. */
+  public T removeFirst() {
+    if (isEmpty()) return null;
+    head = (head + 1) % items.length;
+    T firstItem = items[head];
+    items[head] = null;
+    size -= 1;
 
-    /** Deletes item from back of the list and
-     * returns deleted item. */
-    public T removeLast() {
-        if(isEmpty()) return null;
-        T lastItem = items[backIndex];
-        items[backIndex] = null;
-        size -= 1;
-        if (size == 0) {
-            backIndex = 0;
-            frontIndex = 0;
-            resize(8);
-        }
-        else {
-          backIndex -= 1;
-            if ((size < items.length / 4) && (size > 8) ) {
-                T[] a = (T[]) new Object[items.length / 4];
-                System.arraycopy(items, frontIndex, a, 0, size);
-                items = a;
-                frontIndex = 0;
-                backIndex = size - 1;
-            }
-        }
-        return lastItem;
-    }
+      // resizing
+      if ((size < items.length / 4) && (size > 8)) {
+        resize(items.length / 2);
+      }
 
-    public void printDeque(){
-        if(isEmpty()) {
-            System.out.println("empty");
-            return;
-        }
-        for (int i = 0; i < size; i++) {
-            System.out.print(get(i) + " ");
-        }
-        System.out.println();
-    }
+    return firstItem;
+  }
 
-    public boolean isEmpty(){
-        if (size == 0) return true;
-        return false;
-    }
+  /** Deletes item from back of the list and returns deleted item. */
+  public T removeLast() {
+    if (isEmpty()) return null;
+    tail = ((tail - 1) + items.length) % items.length;
+    T lastItem = items[tail];
+    items[tail] = null;
+    size -= 1;
 
-    /** Gets the ith item in the list from the frontIndex. */
-    public T get(int i) {
-        if(isEmpty()) return null;
-        return items[i + frontIndex];
-    }
+    if ((size < items.length / 4) && (size > 8)) {
+        resize(items.length / 2);
+      }
 
-    /** Returns the number of items in the list. */
-    public int size() {
-        return this.size;
-    }
+    return lastItem;
+  }
 
-    /** Resizes the underlying array to the target capacity. */
-    private void resize(int capacity) {
-        T[] a = (T[]) new Object[capacity];
-        System.arraycopy(items, 0, a, 0, size);
-        items = a;
+  public void printDeque() {
+    if (isEmpty()) {
+      System.out.println("empty");
+      return;
     }
+    for (int i = 0; i < size; i++) {
+      System.out.print(get(i) + " ");
+    }
+    System.out.println();
+  }
+
+  public boolean isEmpty() {
+    if (size == 0) return true;
+    return false;
+  }
+
+  /** Gets the ith item in the list from the frontIndex. */
+  public T get(int i) {
+    if (isEmpty()) return null;
+    return items[i];
+  }
+
+  /** Returns the number of items in the list. */
+  public int size() {
+    return this.size;
+  }
+
+  public int capacity() {
+    return this.items.length;
+  }
+
+  /** Resizes the underlying array to the target capacity. */
+  private void resize(int capacity) {
+    if (capacity < size + 2) capacity = size + 2;
+
+    T[] a = (T[]) new Object[capacity];
+    if (head >= tail && !isEmpty()) {
+      // System.out.println("head: " + head + ", tail: " + tail + ", size: " + size);
+      System.arraycopy(items, head, a, 0, items.length - head);
+      System.arraycopy(items, 0, a, items.length - head, tail);
+    } else {
+      System.arraycopy(items, head, a, 0, size + 1);
+    }
+    items = a;
+    head = 0;
+    tail = size + 1;
+  }
 }
--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -2,87 +2,110 @@ package deque;
 
 import org.junit.Test;
 
-import java.util.Random;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class ArrayDequeTest {
-    @Test
-    public void addLastTest() {
-        ArrayDeque<String> arrDeque = new ArrayDeque<>();
-        for (int i = 0; i < 50; i++) {
-            arrDeque.addLast(String.valueOf(i));
-        }
-        for (int j = 0; j < 45; j++) {
-            arrDeque.removeFirst();
-        }
-        arrDeque.printDeque();
+  @Test
+  public void addLastTest() {
+    ArrayDeque<String> arrDeque = new ArrayDeque<>();
+    for (int i = 0; i < 50; i++) {
+      arrDeque.addLast(String.valueOf(i));
     }
+    for (int j = 0; j < 45; j++) {
+      arrDeque.removeFirst();
+    }
+    arrDeque.printDeque();
+  }
 
-    @Test
-    public void addFirst() {
-        ArrayDeque<String> arrDeque = new ArrayDeque<>();
-        arrDeque.addFirst("1");
-        arrDeque.addFirst("2");
-        arrDeque.addFirst("3");
-    }
+  @Test
+  public void removeLastTest() {
+    ArrayDeque<String> arrDeque = new ArrayDeque<>();
+    arrDeque.addLast("addLast1");
+    arrDeque.addLast("addLast2");
+    assertEquals("addLast2", arrDeque.removeLast());
+    assertEquals("addLast1", arrDeque.removeLast());
+    assertNull(arrDeque.removeLast());
+  }
 
-    @Test
-    public void removeFirstItemSmallTest() {
-        ArrayDeque<String> arrDeque = new ArrayDeque<>();
-        /* add item 0 to 7 from the back of the deque */
-        for (int i = 0; i < 10; i++) {
-            if (Math.random() < 0.5) arrDeque.addFirst(String.valueOf(i));
-            else {
-                arrDeque.addLast(String.valueOf(i));
-            }
-        }
-        arrDeque.printDeque();
-        /* remove all the items from the front of the deque */
-        for (int j = 0; j < 1000; j++) {
-            if (Math.random() < 0.3) arrDeque.removeFirst();
-            else if (Math.random() < 0.6) arrDeque.removeLast();
-            else if (Math.random() < 0.8) arrDeque.addFirst(String.valueOf(j*10));
-            else arrDeque.addLast(String.valueOf(j*20));
-        }
-        arrDeque.printDeque();
-    }
+  @Test
+  public void addFirst() {
+    ArrayDeque<String> arrDeque = new ArrayDeque<>();
+    arrDeque.addFirst("1");
+    arrDeque.addFirst("2");
+    arrDeque.addFirst("3");
+  }
 
-    @Test
-    public void removeFirstItem() {
-        ArrayDeque<String> arrDeque = new ArrayDeque<>();
-        /* add item 0 to 49 from the back of the deque */
-        for (int i = 0; i < 50; i++) {
-            arrDeque.addLast(String.valueOf(i));
-        }
-        arrDeque.printDeque();
-        /* remove item 0 to 41 from the front of the deque */
-        /* check whether arrayDeque resized */
-        for (int j = 0; j < 42; j++) {
-            arrDeque.removeFirst();
-        }
-        arrDeque.printDeque();
-        /* remove item 47 to 49 from the back of the deque */
-        System.out.println("1. Remove last " + arrDeque.removeLast());
-        System.out.println("2. Remove last " + arrDeque.removeLast());
-        System.out.println("3. Remove last " + arrDeque.removeLast());
+  @Test
+  public void removeFillupTest() {
+    ArrayDeque<String> arrDeque = new ArrayDeque<>();
+    /* add item 0 to 7 from the back of the deque */
+    for (int i = 0; i < 10; i++) {
+      if (Math.random() < 0.5) arrDeque.addFirst(String.valueOf(i));
+      else {
+        arrDeque.addLast(String.valueOf(i));
+      }
     }
+    arrDeque.printDeque();
+    /* remove all the items from the front of the deque */
+    for (int j = 0; j < 10000; j++) {
+      if (Math.random() < 0.3) arrDeque.removeFirst();
+      else if (Math.random() < 0.6) arrDeque.removeLast();
+      else if (Math.random() < 0.8) arrDeque.addFirst(j * 10 + "addFirst");
+      else arrDeque.addLast(j * 20 + "addLast");
+    }
+    arrDeque.printDeque();
+  }
 
-    @Test
-    public void addFirstTest() {
-        ArrayDeque<String> arrDeque = new ArrayDeque<>();
-        /* add item 0 to 9 from the back of the deque */
-        for (int i = 0; i < 10; i++) {
-            arrDeque.addLast(String.valueOf(i));
-        }
-        arrDeque.printDeque();
-        /* remove item 0 to 2 from the front of the deque */
-        arrDeque.removeFirst(); // remove "0"
-        arrDeque.removeFirst(); // remove "1"
-        arrDeque.removeFirst(); // remove "2"
-        arrDeque.printDeque();
-        /* add 5 items 10 to 40 from the front of the deque */
-        for (int j = 1; j < 5; j++) {
-            arrDeque.addFirst(String.valueOf(j*10));
-        }
-        arrDeque.printDeque();
+  @Test
+  public void removeFirstItem1() {
+    ArrayDeque<String> arrDeque = new ArrayDeque<>();
+    arrDeque.addFirst("first");
+    arrDeque.addFirst("second");
+    assertEquals("second", arrDeque.removeFirst());
+    assertEquals("first", arrDeque.removeFirst());
+    assertEquals(0, arrDeque.head);
+    assertEquals(1, arrDeque.tail);
+  }
+
+  @Test
+  public void removeFillupAssertTest() {
+    ArrayDeque<Integer> arrDeque = new ArrayDeque<>();
+    for (int i = 0; i < 1000; i++) {
+      arrDeque.addLast(i);
     }
+    System.out.println(arrDeque.capacity());
+    assertEquals(1000, arrDeque.size());
+
+    for (int i = -1; i > -1000; i--) {
+      arrDeque.addFirst(i);
+    }
+    System.out.println(arrDeque.capacity());
+    assertEquals(1999, arrDeque.size());
+
+    for (int i = -999; i < 1000; i++) {
+      Integer first = arrDeque.removeFirst();
+      assertEquals(i, first.intValue());
+    }
+    assertEquals(0, arrDeque.size());
+      System.out.println(arrDeque.capacity());
+  }
+
+  @Test
+  public void removeFillupAssertTest2() {
+    ArrayDeque<Integer> arrDeque = new ArrayDeque<>();
+    for (int i = 0; i < 1000; i++) {
+      arrDeque.addLast(i);
+    }
+    assertEquals(1000, arrDeque.size());
+    for (int i = -1; i > -1000; i--) {
+      arrDeque.addFirst(i);
+    }
+    assertEquals(1999, arrDeque.size());
+    for (int i = 999; i > -1000; i--) {
+      Integer first = arrDeque.removeLast();
+      assertEquals(i, first.intValue());
+    }
+    assertEquals(0, arrDeque.size());
+  }
 }
