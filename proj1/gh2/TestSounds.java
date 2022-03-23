310ebd2aaffbe75a746a387c06cd17f20f3474a2
Jing Ai <ai.jing.cph@gmail.com>
2022-03-23 14:30:31 +0100

    add a few more in proj1

---
+++ proj1/gh2/TestSounds.java
@@ -0,0 +1,26 @@
+package gh2;
+
+import edu.princeton.cs.introcs.StdAudio;
+import org.junit.Test;
+
+public class TestSounds {
+    @Test
+    public void TestHarp() {
+        Harp harpString = new Harp(GuitarHeroLite.CONCERT_A);
+        harpString.pluck();
+        for (int i = 0; i < 50000; i += 1) {
+            StdAudio.play(harpString.sample());
+            harpString.tic();
+        }
+    }
+
+    @Test
+    public void TestDrum() {
+        Drum drum = new Drum(GuitarHeroLite.CONCERT_C);
+        drum.pluck();
+        for (int i = 0; i < 50000; i += 1) {
+            StdAudio.play(drum.sample());
+            drum.tic();
+        }
+    }
+}
