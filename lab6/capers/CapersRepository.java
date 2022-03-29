c04b5d0279736318fa6469a307bf73e52c079061
Jing Ai <ai.jing.cph@gmail.com>
2022-03-29 09:54:06 +0200

    Fill up writeStory

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -1,6 +1,11 @@
 package capers;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import static capers.Dog.DOG_FOLDER;
 import static capers.Utils.*;
 
 /** A repository for Capers 
@@ -18,8 +23,8 @@ public class CapersRepository {
     static final File CWD = new File(System.getProperty("user.dir"));
 
     /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    static final File CAPERS_FOLDER = Utils.join(CWD,".capers");
+    // TODO Hint: look at the `join` function in Utils
 
     /**
      * Does required filesystem operations to allow for persistence.
@@ -32,6 +37,42 @@ public class CapersRepository {
      */
     public static void setupPersistence() {
         // TODO
+        /* check if CAPERS_FOLDER exists
+        * If not, create a new actual folder for it */
+        if (!CAPERS_FOLDER.exists()) {
+            try {
+                CAPERS_FOLDER.mkdir();
+            } catch (SecurityException  e) {
+                e.printStackTrace();
+            }
+        }
+        /* check if DOG_FOLDER exists
+         * If not, create a new actual folder for it */
+        if (!DOG_FOLDER.exists()) {
+            try {
+                DOG_FOLDER.mkdir();
+            } catch (SecurityException  e) {
+                e.printStackTrace();
+            }
+        }
+
+        /* Below is for exercise */
+        /*
+        File outFile = Utils.join(DOG_FOLDER, "dog1.txt");
+        try {
+            ObjectOutputStream out =
+                    new ObjectOutputStream(new FileOutputStream(outFile));
+            out.writeObject(new Dog("Hugo", "333", 2));
+            out.close();
+        } catch (IOException excp) {
+            excp.printStackTrace();
+        }
+
+        File outFile1 = Utils.join(DOG_FOLDER, "story.txt");
+        String currContent = Utils.readContentsAsString(outFile1);
+        Utils.writeContents(outFile1, currContent, "\n", "Hello");
+        */
+
     }
 
     /**
@@ -41,6 +82,18 @@ public class CapersRepository {
      */
     public static void writeStory(String text) {
         // TODO
+        File storyFile = Utils.join(CAPERS_FOLDER, "story");
+        if(!storyFile.exists()) {
+            try {
+                storyFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        String currText = Utils.readContentsAsString(storyFile);
+        Utils.writeContents(storyFile,currText, text, "\n");
+        String newText = Utils.readContentsAsString(storyFile);
+        System.out.println(newText);
     }
 
     /**
